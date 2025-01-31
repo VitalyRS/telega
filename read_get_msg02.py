1d3544b18940bc26735d2ca9f4e79b6adf944955
Vitaly Rudnev <vitaly.rudnev@gmail.com>
2025-01-31 11:29:14 +0100

    Update read_get_msg02.py
    port

--- read_get_msg02.py
+++ read_get_msg02.py
@@ -22,6 +22,8 @@ def echo_message(message):
 
 if __name__ == '__main__':
     print("Starting the bot...")
+    port = int(os.environ.get('PORT', 5000))  # Используйте порт из переменной окружения
+    print(f"Server is running on port {port}")
     try:
         bot.polling(none_stop=True, interval=0)
     except Exception as e:
